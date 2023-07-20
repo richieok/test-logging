4bffc31251377f5d005d1b2dc99f89749f86e1fb
Adolphus Okoro <adolphusokoro@Adolphuss-MacBook-Air.local>
2023-07-20 14:47:51 +0100

    updated

--- src/hooks.server.js
+++ src/hooks.server.js
@@ -1,23 +1,26 @@
-import {logger} from "$lib/logger.js"
+import { logger } from "$lib/logger.js"
 
-export async function handle({event, resolve}){
-    const response = await resolve(event)
-    console.log(event.request.socket);
-    console.log('Client IP address:', getClientIPAddress(event));
-    return response
+export async function handle({ event, resolve }) {
+  const response = await resolve(event)
+  // console.log(event.request.socket);
+  const {ip, from } =  getClientIPAddress(event)
+  let str = `Client IP address: ${ip}`
+  str = from ? str + `. from: ${from}` : str
+  console.log(str);
+  return response
 }
 
 function getClientIPAddress(event) {
-    // Check if IP address is available in the request object
-    if (event.request.connection && event.request.connection.remoteAddress) {
-      return event.request.connection.remoteAddress;
-    }
-    
-    // Check if IP address is available in the headers
-    if (event.request.headers && event.request.headers['x-forwarded-for']) {
-      return event.request.headers['x-forwarded-for'];
-    }
-    
-    // IP address not found
-    return 'Unknown';
+  // Check if IP address is available in the request object
+  if (event.request.connection && event.request.connection.remoteAddress) {
+    return { ip: event.request.connection.remoteAddress, from: "connection.remoteAddress" }
+  }
+
+  // Check if IP address is available in the headers
+  if (event.request.headers && event.request.headers['x-forwarded-for']) {
+    return { ip: event.request.headers['x-forwarded-for'], from: "headers['x-forwarded-for']" };
+  }
+
+  // IP address not found
+  return { ip: 'Unknown', from: null };
 }
